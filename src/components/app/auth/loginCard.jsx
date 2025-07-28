8dc07519cb68980aa247417ccba25b42d378b9fe
kash <wwwkash2@gmail.com>
2025-07-28 20:59:20 +0200

    refactor: divided register and login pages in different smaller components

---
+++ src/components/app/auth/loginCard.jsx
@@ -0,0 +1,126 @@
+// imports
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+  CardAction,
+} from "@/components/ui/card";
+import { useSearchParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { Button } from "@/components/ui/button";
+import { Label } from "@/components/ui/label";
+import { Input } from "@/components/ui/input";
+import { Link, useNavigate } from "react-router-dom";
+import { githubLoginUser, loginUser } from "@/api/auth";
+import { loginErrorHandler } from "@/lib/errorHandlers";
+
+export function LoginCard() {
+  const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
+  const [error, setError] = useState([]);
+  const [username, setUsername] = useState([]);
+  const [password, setPassword] = useState([]);
+
+  useEffect(() => {
+    if (searchParams.get("register")) toast.success("Registration successful");
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const data = await loginUser(username, password);
+
+      // Check error on API call
+      if (data.status === "error") {
+        setError(loginErrorHandler(data));
+        return;
+      }
+
+      navigate("/home");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const handleOnClick = async (e) => {
+    try {
+      githubLoginUser();
+      navigate("/home");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Card className="w-full max-w-sm bg-back ">
+      <CardHeader>
+        <CardTitle>Login to your account</CardTitle>
+        <CardDescription>
+          Enter your username below to login to your account
+        </CardDescription>
+        <CardAction>
+          <Button variant="link">
+            <Link to="/register">Sign Up</Link>
+          </Button>
+        </CardAction>
+      </CardHeader>
+      <CardContent>
+        <form id="login-form" onSubmit={handleSubmit}>
+          <div className="flex flex-col gap-6">
+            <div className="grid gap-2">
+              <Label htmlFor="email">Username</Label>
+              <Input
+                id="email"
+                placeholder="Username"
+                minLength="3"
+                maxLength="12"
+                onChange={(e) => setUsername(e.target.value)}
+                value={username}
+                required
+              />
+            </div>
+            <div className="grid gap-2">
+              <div className="flex items-center">
+                <Label htmlFor="password">Password</Label>
+              </div>
+              <Input
+                id="password"
+                type="password"
+                onChange={(e) => setPassword(e.target.value)}
+                value={password}
+                required
+              />
+            </div>
+          </div>
+        </form>
+      </CardContent>
+      <CardFooter className="flex-col gap-2">
+        {error && <p className="text-xs text-red-500">{error}</p>}
+        <Button
+          type="submit"
+          variant="secondary"
+          className="w-full bg-chart-1"
+          form="login-form"
+        >
+          Login
+        </Button>
+        <Button variant="outline" className="w-full" onClick={handleOnClick}>
+          Login with Github
+          <svg
+            fill="#FFFFFF"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            width="48px"
+            height="48px"
+          >
+            {" "}
+            <path d="M10.9,2.1c-4.6,0.5-8.3,4.2-8.8,8.7c-0.5,4.7,2.2,8.9,6.3,10.5C8.7,21.4,9,21.2,9,20.8v-1.6c0,0-0.4,0.1-0.9,0.1 c-1.4,0-2-1.2-2.1-1.9c-0.1-0.4-0.3-0.7-0.6-1C5.1,16.3,5,16.3,5,16.2C5,16,5.3,16,5.4,16c0.6,0,1.1,0.7,1.3,1c0.5,0.8,1.1,1,1.4,1 c0.4,0,0.7-0.1,0.9-0.2c0.1-0.7,0.4-1.4,1-1.8c-2.3-0.5-4-1.8-4-4c0-1.1,0.5-2.2,1.2-3C7.1,8.8,7,8.3,7,7.6C7,7.2,7,6.6,7.3,6 c0,0,1.4,0,2.8,1.3C10.6,7.1,11.3,7,12,7s1.4,0.1,2,0.3C15.3,6,16.8,6,16.8,6C17,6.6,17,7.2,17,7.6c0,0.8-0.1,1.2-0.2,1.4 c0.7,0.8,1.2,1.8,1.2,3c0,2.2-1.7,3.5-4,4c0.6,0.5,1,1.4,1,2.3v2.6c0,0.3,0.3,0.6,0.7,0.5c3.7-1.5,6.3-5.1,6.3-9.3 C22,6.1,16.9,1.4,10.9,2.1z" />
+          </svg>
+        </Button>
+      </CardFooter>
+    </Card>
+  );
+}
--- src/pages/register.jsx
+++ src/pages/register.jsx
@@ -15,139 +15,14 @@ import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
 import { registerErrorHandler } from "@/lib/errorHandlers";
 import { registerUser } from "@/api/auth";
+import { AuthTitle } from "@/components/app/auth/authTitle";
+import { RegisterCard } from "@/components/app/auth/registerCard";
 
 function Register() {
-  const navigate = useNavigate();
-  const [error, setError] = useState([]);
-  const [username, setUsername] = useState([]);
-  const [password, setPassword] = useState([]);
-  const [confirmPassword, setConfirmPassword] = useState([]);
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const data = await registerUser(username, password, confirmPassword);
-
-      // Check error on API call
-      if (data.status === "error") {
-        setError(registerErrorHandler(data.error));
-        return;
-      }
-
-      navigate("/login?register=true");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
   return (
     <main className="flex flex-col justify-center items-center w-full h-screen bg-background ">
-      <Card className="w-full max-w-sm bg-back flex justify-center items-center mb-5">
-        <CardTitle className="flex items-center gap-3 text-3xl ">
-          <svg
-            width="40"
-            height="40"
-            viewBox="0 0 200 200"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-          >
-            <rect width="200" height="200" rx="30" fill="#111111" />
-            <text
-              x="50%"
-              y="55%"
-              textAnchor="middle"
-              fill="white"
-              fontSize="120"
-              fontFamily="Segoe UI, sans-serif"
-              fontWeight="600"
-              dy=".35em"
-            >
-              K
-            </text>
-          </svg>
-          Kash.social
-        </CardTitle>
-      </Card>
-
-      <Card className="w-full max-w-sm bg-back ">
-        <CardHeader>
-          <CardTitle>Register your new account</CardTitle>
-          <CardDescription>
-            Enter your username below to create your account
-          </CardDescription>
-          <CardAction>
-            <Button variant="link">
-              <Link to="/login">Log In</Link>
-            </Button>
-          </CardAction>
-        </CardHeader>
-        <CardContent>
-          <form id="register-form" onSubmit={handleSubmit}>
-            <div className="flex flex-col gap-6">
-              <div className="grid gap-2">
-                <Label htmlFor="username">Username</Label>
-                <Input
-                  id="username"
-                  type="username"
-                  placeholder="Username"
-                  value={username}
-                  error={error}
-                  onChange={(e) => setUsername(e.target.value)}
-                  minLength="3"
-                  maxLength="12"
-                  required
-                />
-                {error && error.error === "username" && (
-                  <p className="text-xs text-red-500">{error.data}</p>
-                )}
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                </div>
-                <Input
-                  id="password"
-                  type="password"
-                  value={password}
-                  error={error}
-                  onChange={(e) => setPassword(e.target.value)}
-                  minLength="8"
-                  maxLength="64"
-                  pattern="^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[^A-Za-z0-9]).{8,}$"
-                  title="Must contain at least one number, one uppercase letter, one lowercase letter and one symbol"
-                  required
-                />
-              </div>
-              <div className="grid gap-2">
-                <div className="flex items-center">
-                  <Label htmlFor="confirm-password">Confirm password</Label>
-                </div>
-                <Input
-                  id="confirm-password"
-                  type="password"
-                  value={confirmPassword}
-                  onChange={(e) => setConfirmPassword(e.target.value)}
-                  error={error}
-                  required
-                />
-                {error && error.error === "password" && (
-                  <p className="text-xs text-red-500">{error.data}</p>
-                )}
-              </div>
-            </div>
-          </form>
-        </CardContent>
-        <CardFooter className="flex-col gap-2">
-          <Button
-            type="submit"
-            variant="secondary"
-            className="w-full bg-chart-1"
-            form="register-form"
-          >
-            Register
-          </Button>
-        </CardFooter>
-      </Card>
+      <AuthTitle />
+      <RegisterCard />
     </main>
   );
 }
