433858a0ba96bff83184393e385fc995e3b89a5c
kash <wwwkash2@gmail.com>
2025-08-09 00:38:54 +0200

    refactor: reorganised transform functions for clean code

--- src/lib/posts/transformPosts.js
+++ src/lib/posts/transformPosts.js
@@ -1,13 +1,16 @@
 import { dateToText, formatDate } from "@/lib/utils/utils";
 
+// Array of posts
 export const transformPosts = (apiPosts, userId) => {
   return apiPosts.map((post) => ({
-    id: post.authorId,
+    // author data
+    authorId: post.authorId,
     username: post.author.username,
-    joinedAt: dateToText(new Date(post.author.joinedAt)),
     avatar: post.author.avatar,
+    joinedAt: dateToText(new Date(post.author.joinedAt)),
     badgeBool: post.author.github_id ? true : false,
     followBool: post.author.followedBy.some((follow) => follow.id === userId),
+    // post data
     postId: post.id,
     date: formatDate(new Date(post.createdAt)),
     content: post.content,
@@ -17,19 +20,23 @@ export const transformPosts = (apiPosts, userId) => {
   }));
 };
 
-export const transformPost = (apiPost, userId) => {
+// Single post
+export const transformPost = (post, userId) => {
   return {
-    id: apiPost.authorId,
-    username: apiPost.author.username,
-    joinedAt: dateToText(new Date(apiPost.author.joinedAt)),
-    avatar: apiPost.author.avatar,
-    postId: apiPost.id,
-    followBool: apiPost.author.followedBy.some(
-      (follow) => follow.id === userId
-    ),
-    content: apiPost.content,
-    comments: apiPost.comments,
-    likeNumber: apiPost._count.likedBy,
-    commentNumber: apiPost._count.comments,
+    // author data
+    authorId: post.authorId,
+    username: post.author.username,
+    avatar: post.author.avatar,
+    joinedAt: dateToText(new Date(post.author.joinedAt)),
+    badgeBool: post.author.github_id ? true : false,
+    followBool: post.author.followedBy.some((follow) => follow.id === userId),
+    // post data
+    postId: post.id,
+    date: formatDate(new Date(post.createdAt)),
+    content: post.content,
+    likeBool: post.likedBy.some((like) => like.id === userId),
+    likeNumber: post._count.likedBy,
+    likeNumber: post._count.likedBy,
+    commentNumber: post._count.comments,
   };
 };
