937a337508dba97637cb68024f5d7c28e83ad0a8
kash <wwwkash2@gmail.com>
2025-08-09 02:34:40 +0200

    feat: display fetched data

--- src/pages/post.jsx
+++ src/pages/post.jsx
@@ -13,9 +13,9 @@ const Post = () => {
   return (
     <main className="w-full py-10 pr-11 lg:pr-20 lg:pl-9 flex gap-5 min-h-screen">
       <div className="w-full h-fit flex flex-col gap-5">
-        <PostDisplay />
-        <InputComment />
-        <MainComments />
+        <PostDisplay {...post} />
+        <InputComment {...user} />
+        <MainComments comments={comments} />
       </div>
 
       <FeaturedUsers />
