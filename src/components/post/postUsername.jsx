0d6ac26014c1de0f3558819e419c2b8fc6d6fc0a
kash <wwwkash2@gmail.com>
2025-08-09 19:14:24 +0200

    fix: fix visual bugs

--- src/components/post/postUsername.jsx
+++ src/components/post/postUsername.jsx
@@ -7,6 +7,7 @@ import { Button } from "@/components/ui/button";
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar";
 import { followUser, unfollowUser } from "@/api/users/follow";
 import { useEffect, useState } from "react";
+import { Badge, BadgeCheckIcon } from "lucide-react";
 
 export function PostUsername({
   username,
@@ -35,44 +36,47 @@ export function PostUsername({
 
   return (
     <div className="flex justify-between items-center">
-      <HoverCard>
-        <div className="flex  items-center gap-0">
-          <HoverCardTrigger asChild>
-            <Button variant="link">
-              <h1 className="text-xl">{username}</h1>
-            </Button>
-          </HoverCardTrigger>
-        </div>
-        <HoverCardContent className="w-80">
-          <div className="flex items-center gap-1">
-            <Avatar>
-              <AvatarImage src={avatar} />
-              <AvatarFallback>KH</AvatarFallback>
-            </Avatar>
-            <div className="space-y-1">
-              <a className="text-sm font-semibold" href="#">
-                {`@${username}`}
-              </a>
-
-              <div className="text-muted-foreground text-xs">{`Joined ${joinedAt}`}</div>
-            </div>
-            {userId != authorId &&
-              (follow ? (
-                <Button
-                  className="ml-auto"
-                  variant="ghost"
-                  onClick={handleUnfollow}
-                >
-                  Following
-                </Button>
-              ) : (
-                <Button className="ml-auto" onClick={handleFollow}>
-                  Follow
-                </Button>
-              ))}
+      <div className="flex items-center gap-2">
+        <HoverCard>
+          <div className="flex  items-center gap-0">
+            <HoverCardTrigger asChild>
+              <Button variant="link">
+                <h1 className="text-xl">@{username}</h1>
+              </Button>
+            </HoverCardTrigger>
           </div>
-        </HoverCardContent>
-      </HoverCard>
+          <HoverCardContent className="w-80">
+            <div className="flex items-center gap-1">
+              <Avatar>
+                <AvatarImage src={avatar} />
+                <AvatarFallback>KH</AvatarFallback>
+              </Avatar>
+              <div className="space-y-1">
+                <a className="text-sm font-semibold" href="#">
+                  {`@${username}`}
+                </a>
+
+                <div className="text-muted-foreground text-xs">{`Joined ${joinedAt}`}</div>
+              </div>
+
+              {userId != authorId &&
+                (follow ? (
+                  <Button
+                    className="ml-auto"
+                    variant="ghost"
+                    onClick={handleUnfollow}
+                  >
+                    Following
+                  </Button>
+                ) : (
+                  <Button className="ml-auto" onClick={handleFollow}>
+                    Follow
+                  </Button>
+                ))}
+            </div>
+          </HoverCardContent>
+        </HoverCard>
+      </div>
       <p className="text-muted-foreground text-sm">{date}</p>
     </div>
   );
