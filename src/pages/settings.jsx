14c36665bc7cce51599a00dc971914cf120e9e3d
kash <wwwkash2@gmail.com>
2025-08-11 14:49:44 +0200

    feat: create DescriptionEdit and queries to edit description on db

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -1,13 +1,17 @@
 import { AvatarEdit } from "@/components/app/settings/avatarEdit";
 import { UserContext } from "@/App";
 import { useContext } from "react";
+import { DescriptionEdit } from "@/components/app/settings/descriptionEdit";
 
 const Settings = () => {
   const { user } = useContext(UserContext);
 
+  console.log(user);
+
   return (
-    <main className="w-full py-10 pr-11 lg:pr-20 lg:pl-9 flex gap-5 min-h-screen">
+    <main className="w-full py-10 pr-11 lg:pr-20 lg:pl-9 flex flex-col gap-5 min-h-screen">
       <AvatarEdit avatar={user.avatar} />
+      <DescriptionEdit description={user.description} />
     </main>
   );
 };
